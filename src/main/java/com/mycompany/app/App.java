9a8c6170bd4882b3b33ac520044edde4094960da
Yung En <yung-en.ng@keysight.com>
2026-04-06 15:24:42 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is boring, I don't know why I am using it !!!");
+        System.out.println("Modified from local project !");
     }
 
     public String getMessage() {
