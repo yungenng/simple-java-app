df061f41e7fc60bb49872cb7c902509d794577d4
Yung En <yung-en.ng@keysight.com>
2026-04-06 15:27:16 +0800

    My XXXXX change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Modified from local project !");
+        System.out.println("Modified from local project ! 2nd");
     }
 
     public String getMessage() {
