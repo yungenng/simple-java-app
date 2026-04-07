a1a5b5904eb455da9661f11eccca1c3e260cb80b
Yung En <yung-en.ng@keysight.com>
2026-04-07 14:39:12 +0800

    Webhook demo for pipeline

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("New change for Jenkins Hook");
+        System.out.println("Demo webhook with pipeline");
     }
 
     public String getMessage() {
